4c619d1d1560972c6ae0af26090ff56067abe806
omar <omargamer483@gmail.com>
2023-03-24 16:33:57 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-    char str[] = "_putchar";
+    char str[8] = "_putchar";
     int ch;
 
     for (ch = 0; ch < 8; ch++)
 	    _putchar(str[]);
-    _putchar(\n);
+    _putchar("\n");
 
     return (0);
 }
