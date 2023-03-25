bea2f2c083342305c9c3e4592b058d727aabbf74
omar <omargamer483@gmail.com>
2023-03-24 17:21:08 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,11 @@
  */
 void print_alphabet_x10(void)
 {
-int ch;
-for (int i = 0; i < 10; i++)
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
+int ch,i;
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
