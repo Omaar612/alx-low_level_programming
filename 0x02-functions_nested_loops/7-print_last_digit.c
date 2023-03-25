90957baa63c9330359a0d2a66a9624c70089fd2b
omar <omargamer483@gmail.com>
2023-03-25 07:03:59 -0700

    a

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - chack for the last digit.
+ *
+ *@c: input for numbers
+ *
+ * Return: (c)success.
+ */
+int print_last_digit(int c)
+{
+return (c % 10);
+}
