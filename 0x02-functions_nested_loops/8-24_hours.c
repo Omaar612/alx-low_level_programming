5f3d345fc7818d343e87d0ce94120efc6c3e440d
omar <omargamer483@gmail.com>
2023-03-25 07:43:52 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *jack_bauer - print every minute.
+ *
+ * Return: (void)success.
+ */
+void jack_bauer(void)
+{
+int x, y;
+for (int i = 0; i < 25; i++)
+{
+for (int j = 0; j < 60; j++)
+{
+if (i >= 0 && i < 10)
+printf("0");
+if (j >= 0 && j < 10)
+printf("%d:0%d\n", i, j);
+else
+printf("%d:%d\n", i, j);
+}
+}
+}
