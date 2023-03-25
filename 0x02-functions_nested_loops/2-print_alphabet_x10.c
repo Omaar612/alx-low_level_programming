8feff81b2eb0eb210b165f2c98d4f6e6999be7c4
omar <omargamer483@gmail.com>
2023-03-24 17:10:37 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that print the alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int ch;
+for (int i = 0; i < 10; i++)
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
