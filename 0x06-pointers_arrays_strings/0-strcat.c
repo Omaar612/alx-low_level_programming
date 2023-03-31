7415aae4bbdbd81e4df32ec7371332b6ba8e4969
omar <omargamer483@gmail.com>
2023-03-30 17:58:28 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+/**
+ * *_strcat - completing a string with another one
+ *
+ *@dest:the first string.
+ *
+ *@src:the second string.
+ *
+ * Return: Always dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+*dest = *dest + *src;
+return (*dest);
+}
