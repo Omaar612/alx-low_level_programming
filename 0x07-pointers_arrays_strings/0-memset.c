edad50f6a3364ab531bf9347f10559acd50c341a
omar <omargamer483@gmail.com>
2023-03-31 16:09:54 -0700

    m

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i = 0;
-
-    while(i < n)
-    {
-       s[i] = b;
-       i++;
-    }
+int i;
+i = 0;
+while (i < n)
+{
+s[i] = b;
+i++;
+}
 }
