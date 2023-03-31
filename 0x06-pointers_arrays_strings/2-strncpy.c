ebb0b3d5636d9e5a143bc95830a56e631cc6dcef
omar <omargamer483@gmail.com>
2023-03-30 19:29:33 -0700

    a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int j;
+	j = 0;
+	while (src[j] != src[n] && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	dest[j] = '\0';
+return (dest);
+}
