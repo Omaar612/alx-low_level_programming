0315d42cbdec37055266746162647be34663e2e1
omar <omargamer483@gmail.com>
2023-04-03 00:39:05 -0700

    update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,13 +10,12 @@ char *_strpbrk(char *s, char *accept)
 int i;
 while (*s)
 {
-for (i = 0; accept[i] >= '\0'; i++)
+for (i = 0; accept[i]; i++)
+{	
 if (*s == accept[i])
-{
 return (s);
-break;
 }
 s++;
-return ('\0');
 }
+return ('\0');
 }
