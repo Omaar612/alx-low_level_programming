5de4d079c962a914b51bdb53dc7f2d897b9c04f9
omar <omargamer483@gmail.com>
2023-04-03 16:55:47 -0700

    m

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,12 +10,21 @@ char *_strstr(char *haystack, char *needle)
 int i;
 if (*needle == 0)
 return (haystack);
-for (i = 0; haystack[i] >= '\0'; i++)
+for (i = 0; haystack[i] >= '\0';)
 {
 if (needle[i] == haystack[i])
 {
 return (needle);
 }
+do {
+if (needle[i + 1] == '\0')
+{
+return (haystack);
+}
+i++;
+}
+while (haystack[i] == needle[i]);
+haystack++;
 }
 return ('\0');
 }
