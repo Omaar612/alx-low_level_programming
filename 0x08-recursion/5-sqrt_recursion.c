2ed76e31fe4c6d31db2445a3fef1e4912d06a16e
omar <omargamer483@gmail.com>
2023-04-05 04:00:17 -0700

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,5 @@
-#include <stdio.h>
-/**
- * _sqrt1 - recurses to find the natural
- * square root of a number
- * @n: number to calculate the sqaure root of
- * @low: iterator
- * @high: interior
- * Return: the resulting square root
- */
-int _sqrt1(int n, int low, int high)
-{
-if (low > high)
-{
-return (-1);
-}
-int mid;
-mid = (low + high) / 2;
-int square;
-square = mid * mid;
-if (square == n)
-{
-return (mid);
-}
-else if (square < n)
-{
-return (_sqrt1(n, mid + 1, high));
-}
-else
-{
-return (_sqrt1(n, low, mid - 1));
-}
-}
+#include "main.h"
+int _sqrt1(int n, int i);
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root of
@@ -39,15 +9,22 @@ return (_sqrt1(n, low, mid - 1));
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-{
 return (-1);
+return (_sqrt1(n, 0));
 }
-else if (n == 0 || n == 1)
+/**
+ * _sqrt1 - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int _sqrt1(int n, int i)
 {
-return (n);
-}
-else
-{
-return (_sqrt1(n, 1, n));
-}
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (_sqrt1(n, i + 1));
 }
