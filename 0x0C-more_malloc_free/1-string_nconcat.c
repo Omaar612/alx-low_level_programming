fc25578bdd5ae4370673ca30010f1d63c1a32eef
omar <omargamer483@gmail.com>
2023-04-13 05:39:11 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,11 +27,11 @@ else
 s = malloc(1 + len1 + len2);
 if (s == NULL)
 return (NULL);
-for (i = 0; i < len1; i++)
+for (i = 0; i <= len1; i++)
 {
 s[i] = s1[i];
 }
-for (j = 0; i < len2; j++)
+for (j = 0; i <= len2; j++)
 {
 s[i] = s2[j];
 i++;
