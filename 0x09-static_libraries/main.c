7797fa17b432d8615f3fb99e37bbe24b603733d2
omar <omargamer483@gmail.com>
2023-04-15 19:24:27 -0700

    update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,7 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * main - main
- * Return: always 0 (success)
- */
+
 int main(void)
 {
-printf("\"At the end of the day, my goal was to be the best hacker\"");
-printf("\n\t- Kevin Mitnick\n");
-return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
