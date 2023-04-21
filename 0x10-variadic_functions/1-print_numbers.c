81db700001af65aeff0248f353a5184dd6c7b6e9
omar <omargamer483@gmail.com>
2023-04-20 22:43:03 -0700

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ va_start(ptr, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(ptr, int));
-if (separator != NULL && sum != n - 1)
+if (separator != NULL && i != n - 1)
 	printf("%s", separator);
 }
 printf("\n");
